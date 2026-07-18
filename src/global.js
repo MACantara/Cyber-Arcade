7ea50987f218a4074b83527315ed46ab1ed754c2
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2026-07-18 19:14:28 +0800

    Remove legacy challenge.html and relocate legacy source files from root to frontend/public/legacy directory

--- src/global.js
+++
@@ -1,9 +0,0 @@
-(function () {
-
-window.CA = window.CA || {}
-window.CA.services = window.CA.services || {}
-window.CA.CHALLENGE_MANIFESTS = window.CA.CHALLENGE_MANIFESTS || []
-window.CA.labs = window.CA.labs || {}
-
-
-})()
--- src/pages/profile.js
+++
@@ -1,9 +0,0 @@
-(function () {
-
-(async function () {
-  const { loadProfile, loadProgress, loadBadges, loadSettings } = window.CA.services.progress
-  await Promise.all([loadProfile(), loadProgress(), loadBadges(), loadSettings()])
-})()
-
-
-})()
